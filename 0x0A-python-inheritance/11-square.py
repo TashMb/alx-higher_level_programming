e0c5b7d8988ed94c8ac6012b4ea504f00f30b898
TashMb <natashabwalya65@gmail.com>
2023-04-12 23:00:56 +0000

    square two

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -19,24 +19,28 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
+
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
+
     def __str__(self):
         """informal string representation of the rectangle"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-    class Square(Rectangle):
-        """A representation of a square"""
+class Square(Rectangle):
+    """A representation of a square"""
     def __init__(self, size):
         """instantiation of the square"""
         self.integer_validator("size", size)
-        self.__size = sizesuper().__init__(size, size)
+        self.__size = size
+        super().__init__(size, size)
     def area(self):
         """"returns the area of the square"""
         return self.__size ** 2
