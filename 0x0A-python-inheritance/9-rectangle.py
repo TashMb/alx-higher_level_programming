c82a01e5d787f861f57d3a59d3d6e55e65f38ebe
TashMb <natashabwalya65@gmail.com>
2023-04-12 22:27:52 +0000

    full rec

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,11 +23,13 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.__width = width                                                                                                    self.integer_validator("height", height)                                                                                self.__height = height
-                                                                                                                            def area(self):
-                                                                                                                                """returns the area of the rectangle"""
-                                                                                                                                return self.__width * self.__height
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
-                                                                                                                            def __str__(self):
-                                                                                                                                """informal string representation of the rectangle"""
-                                                                                                                                return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
